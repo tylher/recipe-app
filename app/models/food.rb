7111f9cba77c886351b28623c6f57b433ea5c8fc
Damola Adediran <tylher123@gmail.com>
2022-08-03 10:25:26 +0100

    Solve linter errors

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,4 +1,4 @@
 class Food < ApplicationRecord
-  has_many :recipes, class_name: 'Recipe', foreign_key: 'recipe_id',through: :recipe_foods
+  has_many :recipes, class_name: 'Recipe', foreign_key: 'recipe_id', through: :recipe_foods
   has_many :recipe_foods
 end
