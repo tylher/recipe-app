8905313c7100d136721bab66c8b939277cab0c6b
Wuletaw Wonte <wuletawwonte@gmail.com>
2022-08-03 22:51:59 +0300

    Fix route exception adding sysntax error

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :users do
-    resources :foods, :except :update
+    resources :foods, except: :update
     resources :recipes
   end
   # Defines the root path route ("/")
