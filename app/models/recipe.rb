658cd1702d6b9140a34b7177c3ec53dc4659fa2d
Wuletaw Wonte <wuletawwonte@gmail.com>
2022-08-06 15:38:35 +0300

    Fix dependency error

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,5 +1,5 @@
 class Recipe < ApplicationRecord
   belongs_to :user, class_name: 'User', foreign_key: 'user_id'
   has_many :foods, foreign_key: 'food_id', through: :recipe_foods
-  has_many :recipe_foods
+  has_many :recipe_foods, dependent: :delete_all
 end
